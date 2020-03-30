3dbdff4e11296299e832abc58395194b884d31c1
leroylim20-personal <leroylim20@gmail.com>
2020-03-30 16:17:38 +0800

    Updated to include 7 columns for each respective compliance

--- scrapper-main.py
+++ scrapper-main.py
@@ -6,6 +6,15 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+# Compliance Constants
+CIS = 0
+APRA = 1
+MAS = 2
+PCI_DSS = 3
+NIST = 4
+HIPAA = 5
+GDPR = 6
+
 # List all files in a directory using scandir()
 fileNames = [entry.path for entry in os.scandir(".") if entry.is_file()]
 i = 1
@@ -54,13 +63,39 @@ for key, link in resolution_link_df[["Resolution Page"]].iterrows():
     except IndexError:
         resolution = rule.select("#sub-header > div > div > div > div.box.text > p")[0].get_text().strip()
         print("Extraction URL: " + url)
-    resolution_list.append([url, resolution])
+    # Get compliance from URL
+    compliance_list = [0] * 7
+    compliance = rule.select("#sub-header > div > div > div > div.box.text > ul > li")
+    for compliance_item in compliance:
+        compliance_text = compliance_item.get_text().strip()
+        if compliance_text == "The Center of Internet Security AWS Foundations Benchmark":
+            compliance_list[CIS] = 1
+        elif compliance_text == "APRA":
+            compliance_list[APRA] = 1
+        elif compliance_text == "Payment Card Industry Data Security Standard (PCI DSS)":
+            compliance_list[PCI_DSS] = 1
+        elif compliance_text == "MAS":
+            compliance_list[MAS] = 1
+        elif compliance_text == "National Institute of Standards and Technology (NIST)":
+            compliance_list[NIST] = 1
+        elif compliance_text == "Health Insurance Portability and Accountability Act (HIPAA)":
+            compliance_list[HIPAA] = 1
+        elif compliance_text == "General Data Protection Regulation (GDPR)":
+            compliance_list[GDPR] = 1
+        else:
+            print("No labels identified - " + compliance_text)
+    # Append to working list
+    list_item = [url, resolution]
+    list_item.extend(compliance_list)
+    resolution_list.append(list_item)
     print("{:0.2f}%\n".format(key / last_key * 100))
     time.sleep(2)  # Pause to prevent being blocked, 2 secs for good measure
 
 # Merge into df
 print("Exporting...")
-resolution_df = pd.DataFrame(resolution_list, columns=["Resolution Page", "Remediation"])
+resolution_df = pd.DataFrame(resolution_list,
+                             columns=["Resolution Page", "Remediation", "CIS", "APRA", "PCI_DSS", "MAS", "NIST",
+                                      "HIPAA", "GDPR"])
 merged_df = pd.merge(cc_report_failures_df, resolution_df, on="Resolution Page", how="outer")
 # Export to csv
 merged_df.to_csv(output_filename)
